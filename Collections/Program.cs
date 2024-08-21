07aedc4f338dbdfd9f4449e3b7c7778c3d59b0c2
maximuse98 <maximuse98@gmail.com>
2024-08-21 12:37:50 +0300

    Add collections

---
+++ Collections/Program.cs
@@ -0,0 +1,84 @@
+﻿// посилання
+int number;
+int[] numbers;
+
+int x = 10;
+
+int[] xx = new int[10];
+int[] x1 = new int[2] { 10, 15};
+int[] x2 = new int[] { 123, 1233, 35432, 222 };
+int[] x3 = { 15, 23, 45, 89, 12 };
+int[] x4 = [1, 2, 3, 4, 5, 6, 7, 8];
+
+x1[0] = 100;
+Console.WriteLine(x1[0]);
+
+int k = x1[1];
+
+string arrayVal = x1[1].ToString();
+Console.WriteLine(arrayVal);
+
+Console.WriteLine(x1.ToString());
+
+x1[2 - 1] = 1000;
+//x[-1] = 50;
+
+// Index out of bounds
+//x1[5] = 45;
+
+//int ii = -1;
+//x1[ii] = 100;
+
+for (int i = 0; i < x4.Length; i++)
+{
+    Console.Write($"{x4[i]} ");
+}
+Console.WriteLine();
+
+for (int i = x4.Length - 1; i >= 0; i--)
+{
+    Console.Write($"{x4[i]} ");
+}
+Console.WriteLine();
+
+// багатовимірний масив
+int[][] jaggedArray = new int[3][];
+
+jaggedArray[0] = new int[3] { 1, 2, 3 };
+jaggedArray[1] = new int[6] { 4, 5, 6, 0, 55, 45 };
+jaggedArray[2] = new int[3] { 7, 8, 9 };
+
+for (int i = 0; i < jaggedArray.Length; i++)
+{
+    Console.Write("| ");
+    for (int j = 0; j < jaggedArray[i].Length; j++)
+    {
+        Console.Write($"{jaggedArray[i][j]} ");
+    }
+    Console.Write("|");
+    Console.WriteLine();
+}
+
+int[][][] y1 = new int[3][][];
+
+int[,] array2D = new int[3, 2] { { 1, 2 }, { 3, 4 }, { 5, 6 } };
+int[,,] array3D = new int[3, 3, 3]
+    { { { 1,2,3}, { 4,5,6}, { 7,8,9} },
+      { { 10,11,12 }, { 13,14,15 }, { 16,17,18} },
+      { { 19,20,21 }, { 22,23,24 }, { 25,26,27} } };
+
+Console.WriteLine(array3D.Length);
+Console.WriteLine(array3D[1,2,2]);
+
+foreach (int v1 in x4)
+{ 
+    Console.Write($"{v1} ");
+}
+
+foreach (int[] arr in jaggedArray)
+{
+    foreach (int item in arr)
+    {
+        Console.Write($"{item} ");
+    }
+}
--- Cycles/Program.cs
+++ Cycles/Program.cs
@@ -1,88 +1,88 @@
-﻿int x = 0;
-string template = "Pair number: ";
+﻿//int x = 0;
+//string template = "Pair number: ";
 
-int max = 10; //int.Parse(Console.ReadLine());
+//int max = 10; //int.Parse(Console.ReadLine());
 
-// while
-while (x < max - 1)
-{
-    Console.WriteLine($"{template} {x += 2}");
-}
+//// while
+//while (x < max - 1)
+//{
+//    Console.WriteLine($"{template} {x += 2}");
+//}
 
-// do-while
-do
-{
-    Console.WriteLine($"First count");
-} 
-while (x < max - 1);
+//// do-while
+//do
+//{
+//    Console.WriteLine($"First count");
+//} 
+//while (x < max - 1);
 
 
-while (x != 5)
-{
-    x = new Random().Next(0, 6);
-}
+//while (x != 5)
+//{
+//    x = new Random().Next(0, 6);
+//}
 
-// for
-for (int i = 1; i <= max; i++)
-{
-    if (i % 2 == 0)
-    {
-        Console.WriteLine($"{template} {i}");
-    }
-}
+//// for
+//for (int i = 1; i <= max; i++)
+//{
+//    if (i % 2 == 0)
+//    {
+//        Console.WriteLine($"{template} {i}");
+//    }
+//}
 
-Console.WriteLine();
-for (int i = 2; i <= max; i += 2)
-{
-    Console.WriteLine($"{template} {i}");
-}
+//Console.WriteLine();
+//for (int i = 2; i <= max; i += 2)
+//{
+//    Console.WriteLine($"{template} {i}");
+//}
 
-// break - припинення циклу
-while (true)
-{
-    x = new Random().Next(0, 6);
+//// break - припинення циклу
+//while (true)
+//{
+//    x = new Random().Next(0, 6);
 
-    if (x == 5)
-    {
-        break;
-    }
+//    if (x == 5)
+//    {
+//        break;
+//    }
 
-    Console.WriteLine(x);
-}
+//    Console.WriteLine(x);
+//}
 
-// continue - перехід на наступну ітерацію
-for (int i = 1; i <= max; i++)
-{
-    if (i % 2 == 1)
-    {
-        continue;
-    }
+//// continue - перехід на наступну ітерацію
+//for (int i = 1; i <= max; i++)
+//{
+//    if (i % 2 == 1)
+//    {
+//        continue;
+//    }
 
-    Console.WriteLine($"{template} {i}");
-}
+//    Console.WriteLine($"{template} {i}");
+//}
 
-bool isBreak = false;
+//bool isBreak = false;
 
-// goto - перехід на мітку
-for (int i = 0; i < 10; i++)
-{ 
-    for (int j = 0; j < 10; j++)
-    {
-        for (int k = 0; k < 10; k++)
-        {
-            if (i == 2 && j == 4 && k == 5)
-            {
-                goto end;
-            }
-        }
-    }
-}
+//// goto - перехід на мітку
+//for (int i = 0; i < 10; i++)
+//{ 
+//    for (int j = 0; j < 10; j++)
+//    {
+//        for (int k = 0; k < 10; k++)
+//        {
+//            if (i == 2 && j == 4 && k == 5)
+//            {
+//                goto end;
+//            }
+//        }
+//    }
+//}
 
-end:
-Console.WriteLine("End of the program.");
+//end:
+//Console.WriteLine("End of the program.");
 
-// return - вихід з програми (тимчасово!)
-return;
+//// return - вихід з програми (тимчасово!)
+//return;
 
 
 // input: 4
@@ -97,3 +97,51 @@ return;
 // 3 * 1 = 3
 // 3 * 2 = 6
 // 3 * 3 = 9
+
+// 11
+// 12
+// 13
+// 14
+
+//int sum;
+//int num;
+
+//for (int i = 1; i < 10; i++)
+//{
+//    for (int j = 0; j < 10; j++)
+//    {
+//        //if ((i*i + j*j) % 13 == 0)
+//        //{
+//        //    Console.WriteLine($"{i}{j}");
+//        //}
+
+//        sum = i + j;
+//        num = i * 10 + j;
+
+//        if (sum + Math.Pow(sum, 2) == num)
+//        { 
+//            Console.WriteLine(num); 
+//        }
+//    }
+//}
+
+//int rows = 10;
+//bool val;
+
+//for (int i = 1; i <= rows; i++)
+//{
+//    val = i % 2 != 0;
+
+//    for (int j = 0; j < i; j++)
+//    {
+//        Console.Write(val ? "1" : "0");
+//        val = !val;
+//    }
+//    Console.WriteLine();
+//}
+
+using Cycles;
+
+Calc calc = new Calc();
+
+calc.Do();
