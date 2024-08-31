e046593e6e9bd955e45e45d0678e2448b1c03e06
maximuse98 <maximuse98@gmail.com>
2024-09-01 01:05:42 +0300

    Add collections 2

---
+++ Collections2/Program.cs
@@ -0,0 +1,122 @@
+﻿List<string> listOfStrings = new List<string>();
+
+listOfStrings.Add("First string");
+listOfStrings.Add("Second string");
+
+//Console.WriteLine(listOfStrings[1]);
+
+string[] strings = { "Third string", "Fourth string" };
+
+listOfStrings.AddRange(strings);
+
+foreach (string str in listOfStrings)
+{
+    Console.WriteLine(str);
+}
+
+List<int> ints = new List<int>() { 1, 2, 3, 4, 5, 6 };
+List<int> ints2 = new List<int>(ints);
+
+ints.Add(7);
+ints.Remove(2);
+
+bool is50Removed = ints.Remove(50);
+ints.RemoveAt(2);
+
+int sizeOfList = ints.Count();
+
+int[] ints1 = ints.ToArray();
+
+List<int> list = ints1.ToList();
+
+// Task 1
+string input = "It`s Friday, dudes! 33";
+//string input = "Lorem 1 ipsum dolor sit amet, consectetur 8 adipiscing elit 9, sed 3 do eiusmod@ tempor incididunt 5 ut labore** et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+string[] words = input.Split(' ');
+
+Console.WriteLine(words.Length);
+
+// linq
+// int count = input.Where(c => c == ' ').Count();
+// input.Count(x => x == ' ');
+
+// Task 2
+int start = 2;
+int lengthOfSubstring = 5;
+
+for (int i = start; i < start + lengthOfSubstring; i++)
+{
+    Console.Write(input[i]);
+}
+Console.WriteLine();
+
+// Task 3
+char[] spec = { '!', '@', '*', '.', ',', ' ', '`'};
+int numberOfLetters = 0;
+int numberOfSpec = 0;
+int numberOfDigits = 0;
+
+//foreach (char c in input)
+//{
+//    // check for digits
+//    try
+//    {
+//        Convert.ToInt32(c.ToString());
+//        numberOfDigits++;
+
+//        continue;
+//    }
+//    catch { }
+
+//    // check for special characters
+//    if (spec.Contains(c))
+//    {
+//        numberOfSpec++;
+
+//        continue;
+//    }
+
+//    // check for letters
+//    numberOfLetters++;
+//}
+
+Console.WriteLine($"Input: {input}");
+Console.WriteLine($"Letters count: {numberOfLetters}");
+Console.WriteLine($"Special characters count: {numberOfSpec}");
+Console.WriteLine($"Digits count: {numberOfDigits}");
+
+Console.WriteLine();
+
+// Task 4
+bool[] mask = new bool[input.Length];
+int maxCount = 0;
+char resultChar = '^';
+
+for (int i = 0; i < input.Length; i++)
+{
+    if (mask[i] || input[i] == ' ')
+    {
+        continue;
+    }
+
+    char currentChar = input[i];
+    int currentCharCount = 1;
+
+    for (int j = i + 1; j < input.Length; j++)
+    {
+        if (input[j] == currentChar)
+        {
+            currentCharCount++;
+            mask[j] = true;
+        }
+    }
+
+    if (currentCharCount > maxCount)
+    {
+        maxCount = currentCharCount;
+        resultChar = currentChar;
+    }
+}
+
+Console.WriteLine($"Most common character: {resultChar}");
+Console.WriteLine($"Count: {maxCount}");
---
+++ Strings/Program.cs
@@ -0,0 +1,93 @@
+﻿//string str = "Hello, World!";
+
+//str[1] = 'a';
+
+using System.Text;
+
+int a;
+bool b;
+string str = string.Empty; //= "";
+
+char[] ch = { 'H', 'e', 'l', 'l', 'o' };
+string str2 = new string(ch);
+
+Console.WriteLine(str);
+
+str = "welcome";
+Console.WriteLine(str);
+
+str = string.Empty;
+
+string formattedString = $"Value: {str}";
+formattedString = string.Format("Value 1: {0}, Value 2: {1}, Value 3: {2}", str, "he-he", "ha-ha");
+
+Console.WriteLine(formattedString);
+
+Console.WriteLine("Value 1: " + str + ", Value 2: " + "he-he" + ", Value 3: " + "ha-ha");
+formattedString += "!";
+
+string name = "Maxim";
+string surname = "Kabakov";
+
+string result = "Welcome, " + name + " " + surname + "!";
+
+// "0123456789"
+
+string result2 = string.Empty;
+for (int i = 0; i < 10; i++)
+{
+    result2 += i.ToString();
+}
+Console.WriteLine(result2);
+
+StringBuilder stringBuilder = new StringBuilder();
+for (int i=0; i< 10; i++)
+{
+    stringBuilder.Append(i);
+}
+Console.WriteLine(stringBuilder.ToString());
+
+Console.WriteLine(stringBuilder[2]);
+//Console.WriteLine(stringBuilder[15]);
+
+string s1 = "Hello";
+string s2 = "Hello";
+string s3 = str;
+
+// s2 == s1; //true
+// s3 == str; //true
+
+s2 = "World";
+
+// s2 != s1; //true
+
+// конкатенація
+string.Concat(s1, s2, s1, s1, s1,s1,s1,s1,s1,s1,s1,s1,s1);
+// підстрока
+//s1[..1]; // s1[..1]
+string s4 = s1.Substring(1, 3); // s1[1..4]
+
+// розділення
+Console.WriteLine("Split:");
+string[] formattedValuesArray = formattedString.Split(',', 4, StringSplitOptions.None);
+foreach (var item in formattedValuesArray)
+{
+    Console.WriteLine(item);
+}
+
+// конвертація
+int number = 123;
+string numberStr = number.ToString();
+
+int res = int.Parse(numberStr);
+
+string str4 = "C:\\Users\\maxim.kabakov\\Videos\\Captures\n\n\tEnd";
+Console.WriteLine(str4);
+
+string unicode = "\ud83d\udc02";
+
+Console.WriteLine(unicode);
+
+string str5 = @"C:\Users\maxim.kabakov\Videos\Captures\n\n\tEnd";
+
+// linq
