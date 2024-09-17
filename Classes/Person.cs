8570624ac2b4e8745849ba034ed884b100566676
maxim.kabakov <maxim.kabakov@hys-enterprise.com>
2024-09-17 19:07:26 +0300

    Add Classes

---
+++ Classes/Person.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes
+{
+    public class Person
+    {
+        // поля
+        private string name;
+        private string grade;
+        private int[] marks;
+        private string gender;
+        private int age;
+
+        // властивості
+        public int Age 
+        {   
+            get { return age; } 
+            set 
+            {
+                if (value > 5)
+                {
+                    age = value;
+                }
+            } 
+        }
+
+        // конструктор
+        public Person(string name, string gender)
+        { 
+            this.name = name;
+            this.gender = gender;
+        }
+
+        public Person(string name, string gender, string grade) : this(name, gender)
+        { 
+            this.grade = grade;
+        }
+
+        // методи
+        public void Introduce()
+        {
+            Console.WriteLine($"Hi, my name is {name}");
+        }
+
+        public void Speak()
+        {
+            Introduce();
+
+            if ( grade != null)
+            {
+                Console.WriteLine($"I study at {grade}");
+            }
+        }
+
+        public void SetName(string name)
+        {
+            if (name != null)
+            { 
+                this.name = name;
+            }
+        }
+
+        public string GetName()
+        {
+            string prefix;
+            if (gender == "M")
+            { 
+                prefix = "Mr. ";
+            }
+            else
+            {
+                prefix = "Ms. ";
+            }
+            return prefix + name;
+        }
+
+        public void SetGrade(string grade)
+        {
+            this.grade = grade;
+        }
+
+        public string GetGrade()
+        {
+            return grade;
+        }
+    }
+}
---
+++ Classes/Program.cs
@@ -0,0 +1,38 @@
+﻿using Classes;
+using System.Diagnostics;
+using System.Xml.Linq;
+
+Person person = new Person("John", "M");
+person.Speak();
+
+//person.name = "John";
+
+Person person1 = new Person("Jane", "F");
+//person1.name = "Jane";
+
+//Console.WriteLine(person.name + " " + person1.name);
+
+//Person.Speak(); -  not allowed
+
+string[] names = new string[10];
+string[] grades = new string[10];
+int[][] marks = new int[10][];
+
+Person[] people = { person, person1, new Person("Peter", "M"), new Person("Lisa", "M", "5-A") };
+
+foreach (Person p in people)
+{
+    p.Speak();
+}
+
+people[0].SetName("Mark");
+Console.WriteLine(people[0].GetName());
+
+people[0].Age = 14;
+int firstAge = people[0].Age;
+
+List<string> strings = new List<string>();
+
+strings.Add("John");
+strings.Add("Jane");
+strings.Add("Peter");
